913dc25153ca5ff0641cc35e9a20ed71cf3d1440
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-11-06 00:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let selectDiv = document.getElementById("colorSelect");
+let colors = ['Red', 'Green', 'White', 'Black']
+function deleted(){
+    let color = selectDiv.value;
+    colors = colors.filter( (item)=> item!=color)
+    selectDiv.innerHTML = ""
+    colors.map((item)=>{
+        selectDiv.innerHTML += `
+            <option value=${item}>${item}</option> 
+
+        `
+    })
+
+    
+
+
+}
